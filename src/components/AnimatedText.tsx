dd5028be36ed8485ff4f35f29ff597e2c8118592
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-15 16:30:44 +0200

    fix: remove duplicate letters

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -44,17 +44,17 @@ export const AppearingText = ({ texts, slices }: { texts: string[], slices?: num
             Math.floor((pCur * 3) * (it.length - 1)),
             (slices || [])[curText] || 0
         );
+
         const rand = it.split('').slice(
             start
         )
-
 
         rand.sort((a, b) => {
             return ((Math.random() - 0.5) * (1 - pCur)) * 2 + ((pCur) * (it.indexOf(a) - it.indexOf(b)))
         }).join('');
 
         const part1 = it.split('').slice((slices || [])[curText] || 0);
-        const part = part1.map((_, i) => (Math.floor((pCur * 3) * (it.length - 1)) > i ? part1[i] : rand[i - start])).join('');
+        const part = part1.map((_, i) => (Math.floor((pCur * 2) * (it.length - 1)) > i ? part1[i] : rand[i - start])).join('');
         const txt = it.slice(0, (slices || [])[curText] || 0) + part;
         setText(txt)
         setStart(start);
