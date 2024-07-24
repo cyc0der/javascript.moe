3974617b467a01cb0cae1f214d6b9cc838482f32
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-24 22:40:22 +0200

    chore: remove logs

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -36,8 +36,8 @@ export const MyName = () => {
     if (hRef.current) {
         marginBottom = +window.getComputedStyle(hRef.current).marginBottom.replace('px', '') / window.devicePixelRatio;
     }
+    
     const offset = marginBottom;
-    console.log("OFF", offset)
     const y = useParallax(scrollYProgress, ratio - offset, ratio - offset, easeInOut)
     const opacity = useTransform(scrollYProgress, [0.75, 1], [1, 0.1]);
     const fS = screen.width <= 452 ? 52 : 72;
