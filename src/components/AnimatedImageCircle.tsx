ed36588d3cccf66a1072208e6b7da9826812090d
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-24 23:39:44 +0200

    fix: improve animation

--- src/components/AnimatedImageCircle.tsx
+++ src/components/AnimatedImageCircle.tsx
@@ -6,11 +6,17 @@ import { sectionCtx } from './AnimatedSection';
 export type AnimatedImageCircleProps = {
     src: string;
     className?: string
-    size?: 'sm' | 'md'
+    size?: AnimatedImageCircleSize
 }
-const heights: Record<string, number> = {
+
+const heights: Record<AnimatedImageCircleSize, number> = {
     sm: 320,
 }
+
+export enum AnimatedImageCircleSize {
+    sm = 'sm'
+}
+
 export const AnimatedImageCircle = ({ src, className, size = 'sm' }: AnimatedImageCircleProps) => {
     const clsn = clsx("rounded-full overflow-hidden my-auto", className)
     const imgHeight = heights[size];
@@ -21,17 +27,9 @@ export const AnimatedImageCircle = ({ src, className, size = 'sm' }: AnimatedIma
         target: ref || undefined,
         offset: ["start start", "end end"]
     });
-    // const par = useParallax(scrollYProgress, imgHeight * 0.5, imgHeight * 0.5)
-    // const x = useTransform(scrollYProgress, [0.25, 1], ["0vw", "-45vw"])
 
     const imgFilter = useTransform(scrollYProgress, [0, 0.25], ["saturate(100%)", "saturate(0%)"])
-
-
     const opacity = useTransform(scrollYProgress, [0.25, 0.5], ["100%", "0%"])
-
-    const out = useTransform(scrollYProgress, [0.25, 0.5], [0, -screen.height]);
-
-    // const y = useTransform(() => par.get() + out.get())
 
     return <motion.div className={clsn} style={{
         borderRadius: '1000px',
@@ -42,12 +40,7 @@ export const AnimatedImageCircle = ({ src, className, size = 'sm' }: AnimatedIma
         boxShadow: '0px 0px 6px 1px black',
         opacity,
         zIndex: 100,
-        // y,
-        // x,
-        // scale,
-        // position: 'absolute',
     }}>
         <div style={{ background: `url(${src}) center / cover`, height: '100%' }} />
-
     </motion.div>
 }
