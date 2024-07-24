42ab1258b3f329cc5bb45329373e86fa9d5f5bc9
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-24 02:37:34 +0200

    fix: improve animation

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -34,7 +34,7 @@ export const MyName = () => {
     const ratio = (height / 1.75) / 4;
     let marginBottom = 16;
     if (hRef.current) {
-        marginBottom = +window.getComputedStyle(hRef.current).marginBottom.replace('px', '');
+        marginBottom = +window.getComputedStyle(hRef.current).marginBottom.replace('px', '') / window.devicePixelRatio;
     }
     const offset = marginBottom;
     console.log("OFF", offset)
