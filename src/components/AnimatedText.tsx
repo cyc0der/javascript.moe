3c23030e8983061b3fc386226fc1b1e87b01990f
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-25 10:17:15 +0200

    fix: remove top css

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -95,7 +95,7 @@ export const AppearingText = ({ texts, slices }: { texts: string[], slices?: num
         target: ref || undefined,
         offset: ["start start", "end end"]
     });
-    const y = useParallax(scrollYProgress, 110 , 110 - screen.height * 0.25, easeInOut)
+    const y = useParallax(scrollYProgress, 110, 110 - (screen.height / window.devicePixelRatio) * 0.25, easeInOut)
     const t2 = useTransform(scrollYProgress, [0, 1], [1, texts.length + 1])
     const boxShadow = useTransform(scrollYProgress, [0, 1], ['0px 0px 0px black', '0px 0px 12px black'])
 
