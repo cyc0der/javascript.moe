bccf31fdc2486b5d181f3a7223ea57f55e27cf9d
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-26 16:16:47 +0200

    fix: add missing files

---
+++ src/lib/const.ts
@@ -0,0 +1,9 @@
+export const ABOUT_TEXT = `
+I'm Moe. A guy in his 30ies. I have 11 years of professional experience with JavaScript. 
+Over the years I gained expert knowledge in web and fullstack development. 
+Most of my time I'm working with TypeScript, React and GraphQL.
+As a senior TypeScript developer, I value code quality, static typing and the right 
+tooling. If you're looking for a senior frontend developer or a lead developer 
+in the greater area of Freiburg im Breisgau (or remote), don't hesitate to contact me. 
+I'm well experienced in working with remote teams following Scrum principles.
+`;
---
+++ src/lib/types.ts
@@ -0,0 +1,3 @@
+export type AboutSectionProps = {
+  text: string;
+};
