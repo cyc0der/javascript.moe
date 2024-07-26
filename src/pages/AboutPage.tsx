7209e5b55d8c6e993b7b02c23f0d6443362d11f9
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-26 15:30:03 +0200

    fix: make panel absolute

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -36,9 +36,9 @@ export const AboutSection = () => {
     const dist = getVH(50)
     const offset = -dist;
     const blur = useTransform(scrollYProgress, [0, 1], ['blur(4px)', 'blur(0px)'])
-    const rblur = useTransform(scrollYProgress, [0, 1], ['brightness(100%) blur(0px) saturate(100%)', 'brightness(80%) blur(4px) saturate(140%)'])
-    const background = useTransform(scrollYProgress, [0, 1], ['#FFFFFF11', '#00000033'])
-    const overflowY = useTransform(scrollYProgress, [0, 1], ['hidden', 'auto']);
+    // const rblur = useTransform(scrollYProgress, [0, 1], ['brightness(100%) blur(0px) saturate(100%)', 'brightness(80%) blur(4px) saturate(140%)'])
+    // const background = useTransform(scrollYProgress, [0, 1], ['#FFFFFF11', '#00000033'])
+    // const overflowY = useTransform(scrollYProgress, [0, 1], ['hidden', 'auto']);
 
     return <>
         <BackgroundImage src="/images/wallpaper/5.webp" alt="Seepark in Freiburg" />
@@ -54,8 +54,12 @@ export const AboutSection = () => {
                     onClick={scrollToTop}
                 >
                     <motion.div
-                        style={{ background, backdropFilter: rblur, overflowY }}
-                        className="absolute w-[80ch] top-0 p-4 rounded-md shadow-lg shadow-black max-h-[calc(100lvh-120px)]" >
+                        style={{ 
+                            // background, 
+                            // backdropFilter: rblur, 
+                            // overflowY 
+                        }}
+                        className="p-4 rounded-md shadow-lg shadow-black max-h-[calc(100lvh-120px)]" >
                         <motion.p style={{ filter: blur, textShadow: '0px 0px 1px black' }}>{text}</motion.p>
                     </motion.div>
                 </button>
