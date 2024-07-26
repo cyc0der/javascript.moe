9af29fda17e8fd788d34a76ffb1e1463db02c137
Moritz Roessler <moritz.roessler@gmail.com>
2024-07-26 03:29:22 +0200

    fix: test

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -10,15 +10,15 @@ import { BackgroundImage } from "../components/BackgroundImage"
 // import { Link } from "react-router-dom"
 // import { useContext } from "react";
 
-const text = `
-I'm Moe. A guy in his 30ies. I have 11 years of professional experience with JavaScript. 
-Over the years I gained expert knowledge in web and fullstack development. 
-Most of my time I'm working with TypeScript, React and GraphQL.
-As a senior TypeScript developer, I value code quality, static typing and the right 
-tooling. If you're looking for a senior frontend developer or a lead developer 
-in the greater area of Freiburg im Breisgau (or remote), don't hesitate to contact me. 
-I'm well experienced in working with remote teams following Scrum principles.
-`
+// const text = `
+// I'm Moe. A guy in his 30ies. I have 11 years of professional experience with JavaScript. 
+// Over the years I gained expert knowledge in web and fullstack development. 
+// Most of my time I'm working with TypeScript, React and GraphQL.
+// As a senior TypeScript developer, I value code quality, static typing and the right 
+// tooling. If you're looking for a senior frontend developer or a lead developer 
+// in the greater area of Freiburg im Breisgau (or remote), don't hesitate to contact me. 
+// I'm well experienced in working with remote teams following Scrum principles.
+// `
 
 export const AboutPage = () => {
 
