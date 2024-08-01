0dc274886b148694a4daa3d15f51a9d430552cbc
Moritz Roessler <moritz.roessler@gmail.com>
2024-08-01 13:32:39 +0200

    fix: add sw cache

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,21 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import viteCompression from "vite-plugin-compression";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths(), viteCompression()],
+  plugins: [
+    svgr(),
+    react(),
+    tsconfigPaths(),
+    viteCompression(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      },
+    }),
+  ],
   envPrefix: "VITE_",
 });
