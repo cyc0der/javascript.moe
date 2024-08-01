99d19924ed4ce835403ed70be3a8e05b97805d7b
Moritz Roessler <moritz.roessler@gmail.com>
2024-08-01 17:36:16 +0200

    feat: refine content

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,4 +1,4 @@
-export const ABOUT_TEXT = `
+export const ABOUT_TEXT_ME = `
 Hi, I'm Moritz (Moe). A passionate web developer in his 30ies. 
 I have 12+ years of professional experience with JavaScript. 
 Over the years I gained expert knowledge in web and fullstack development. 
@@ -7,4 +7,12 @@ As a senior TypeScript developer, I value code quality, static typing and the ri
 tooling. I am used to Scrum and experienced in realizing sophisticated projects for big clients. 
 If you're looking for a senior frontend developer or a lead developer in the 
 greater area of Freiburg im Breisgau (or remote), don't hesitate to contact me. 
+`;
+
+export const ABOUT_TEXT = `
+I'm Moritz Roessler, a senior TypeScript developer with over 12 years of experience 
+in web and full-stack development. I specialize in JavaScript, TypeScript, React, and GraphQL, 
+focusing on code quality and optimal tooling. 
+My work includes delivering complex projects for large clients, utilizing agile frameworks like Scrum. 
+If you're looking for a lead or senior frontend developer in Freiburg im Breisgau or for remote work, feel free to contact me.
 `;
